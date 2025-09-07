b769b29c9682a5cb586a67f4ce21f2875269f9b4
flycloud <3012287203@qq.com>
2025-09-07 21:45:57 +0800

    Submit a manually written branch

--- HarmlessCola/Config.cs
+++ HarmlessCola/Config.cs
@@ -1,85 +1,27 @@
 using System.ComponentModel;
 
-namespace HarmlessCola
+namespace HarmlessCola;
+
+public class Config
 {
-    /// <summary>
-    /// Configuration class for HarmlessCola plugin
-    /// Contains all configurable options to customize plugin behavior
-    /// </summary>
-    public class Config
-    {
-        /// <summary>
-        /// Gets or sets whether the plugin is enabled
-        /// </summary>
-        [Description("Is the plugin enabled?")]
-        public bool PluginEnable { get; set; } = true;
-
-        /// <summary>
-        /// Gets or sets whether to display normal logs
-        /// Used to record basic operation information of the plugin
-        /// </summary>
-        [Description("Do you want to display the log?")]
-        public bool LogEnable { get; set; } = true;
-
-        /// <summary>
-        /// Gets or sets whether to display debug logs
-        /// Debug logs contain more detailed information, mainly used for development and troubleshooting
-        /// </summary>
-        [Description("Do you want to display the debug log? (Suggest not displaying it as it is redundant and useless)")]
-        public bool DebugLogEnable { get; set; } = false;
-
-        /// <summary>
-        /// Gets or sets whether to send messages when players use SCP-207
-        /// </summary>
-        [Description("Do you want to send a message when a player uses SCP-207?")]
-        public bool MessageEnable { get; set; } = true;
-
-        /// <summary>
-        /// Gets or sets the message display type
-        /// Supports "Broadcast" and "Hint" types
-        /// </summary>
-        [Description("How to display this message? (Broadcast/Hint)")]
-        public string MessageType { get; set; } = "Broadcast";
-
-        /// <summary>
-        /// Gets or sets the message content to send
-        /// You can use [player] placeholder to display player name
-        /// </summary>
-        [Description("What message do you want to send? (Use [player] for the player's name)")]
-        public string MessageContent { get; set; } = "Our server has enabled Coke No Harm, CHARGE!!!";
-
-        /// <summary>
-        /// Gets or sets the message display duration (seconds)
-        /// Only effective for Broadcast and Hint type messages
-        /// </summary>
-        [Description("How long do you want the message to be displayed in seconds?")]
-        public ushort MessageDuration { get; set; } = 3;
-
-        /// <summary>
-        /// Validates whether the configuration is valid
-        /// </summary>
-        /// <returns>Returns true if configuration is valid, otherwise false</returns>
-        public bool IsValid()
-        {
-            // Check if message type is supported
-            if (MessageEnable && !IsValidMessageType(MessageType))
-                return false;
-
-            // Check if message content is empty
-            if (MessageEnable && string.IsNullOrWhiteSpace(MessageContent))
-                return false;
-
-            return true;
-        }
-
-        /// <summary>
-        /// Checks if the message type is valid
-        /// </summary>
-        /// <param name="messageType">The message type to check</param>
-        /// <returns>Returns true if message type is valid</returns>
-        private static bool IsValidMessageType(string messageType)
-        {
-            return messageType is "Broadcast" or "Hint";
-        }
-    }
+    [Description("Is the plugin enabled?")]
+    public bool PluginEnable { get; set; } = true;
+    
+    [Description("Do you want to display the log?")]
+    public bool LogEnable { get; set; } = true;
+    
+    [Description("Do you want to display the debug log? (Suggest not displaying it as it is redundant and useless)")]
+    public bool DebugLogEnable { get; set; } = false;
+    
+    [Description("Do you want to send a message when a player uses SCP-207?")]
+    public bool MessageEnable { get; set; } = true;
+    
+    [Description("How to display this message? (Broadcast/Hint)")]
+    public string MessageType { get; set; } = "Broadcast";
+    
+    [Description("What message do you want to send? (Use [player] for the player's name)")]
+    public string MessageContent { get; set; } = "Our server has enabled Coke No Harm, CHARGE!!!";
+    
+    [Description("How long do you want the message to be displayed in seconds?")]
+    public ushort MessageDuration { get; set; } = 3;
 }
--- HarmlessCola/PluginEntry.cs
+++ HarmlessCola/PluginEntry.cs
@@ -1,4 +1,5 @@
 using System;
+using LabApi.Events;
 using LabApi.Events.CustomHandlers;
 using LabApi.Features;
 using LabApi.Features.Console;
@@ -7,251 +8,74 @@ using LabApi.Loader.Features.Plugins;
 
 namespace HarmlessCola
 {
-    /// <summary>
-    /// Main entry class for HarmlessCola plugin
-    /// Inherits from Plugin base class, responsible for managing plugin lifecycle, configuration loading and event registration
-    /// </summary>
     public class HarmlessColaPlugin : Plugin
     {
-        #region Plugin Information
-        
-        /// <summary>
-        /// Static instance of the plugin for global access
-        /// Uses singleton pattern to ensure only one plugin instance
-        /// </summary>
         public static HarmlessColaPlugin Instance { get; private set; }
 
-        /// <summary>
-        /// Plugin name
-        /// </summary>
         public override string Name => "HarmlessCola";
 
-        /// <summary>
-        /// Plugin description
-        /// </summary>
-        public override string Description => "Ensure that Coca Cola (SCP-207) no longer causes harm to players";
+        public override string Description => "Ensure that Coca Cola (SCP-207) no longer causes harm";
 
-        /// <summary>
-        /// Plugin author
-        /// </summary>
         public override string Author => "FlyCloud";
 
-        /// <summary>
-        /// Plugin version
-        /// Uses semantic versioning: major.minor.patch.build
-        /// </summary>
-        public override Version Version => new(1, 0, 0, 0);
+        public override Version Version => new(1,0,0,0);
 
-        /// <summary>
-        /// Required LabApi version
-        /// </summary>
         public override Version RequiredApiVersion => new(LabApiProperties.CompiledVersion);
 
-        #endregion
-
-        #region Configuration and State
-
-        /// <summary>
-        /// Plugin configuration instance containing all configurable options
-        /// </summary>
-        public static Config Config { get; private set; }
+        public static Config Config;
         
-        /// <summary>
-        /// Flag indicating if configuration has errors
-        /// If configuration file format is wrong or validation fails, this flag will be true
-        /// </summary>
-        private bool _hasConfigurationErrors;
-
-        #endregion
-
-        #region Event Handlers
-
-        /// <summary>
-        /// SCP-207 damage exemption event handler
-        /// Responsible for intercepting and preventing SCP-207 damage to players
-        /// </summary>
-        public ExemptDamageEventsHandler ExemptDamageEventHandler { get; } = new();
+        private bool _hasIncorrectSettings;
         
-        /// <summary>
-        /// Message sending event handler
-        /// Responsible for sending configured messages when players use SCP-207
-        /// </summary>
-        public SendMessageEventsHandler SendMessageEventHandler { get; } = new();
-
-        #endregion
-
-        #region Plugin Lifecycle Methods
-
-        /// <summary>
-        /// Load plugin configuration
-        /// Called before plugin is enabled, responsible for reading and validating configuration file
-        /// </summary>
+        public ExemptDamageEventsHandler ExemptDamageEvent { get; } = new ();
+        
+        public SendMessageEventsHandler SendMessageEvent { get; } = new ();
+        
         public override void LoadConfigs()
         {
             base.LoadConfigs();
-            
-            try
-            {
-                // Attempt to load configuration file
-                Config tempConfig;
-                _hasConfigurationErrors = !this.TryLoadConfig("Config.yml", out tempConfig);
-                Config = tempConfig;
-                
-                if (_hasConfigurationErrors || Config == null)
-                {
-                    Logger.Error("Failed to load configuration file 'Config.yml'");
-                    return;
-                }
+            _hasIncorrectSettings = !this.TryLoadConfig("Config.yml", out Config);
+            if(_hasIncorrectSettings || Config == null) return;
 
-                // Validate configuration validity
-                if (!Config.IsValid())
-                {
-                    Logger.Error("Configuration validation failed. Please check your config settings.");
-                    _hasConfigurationErrors = true;
-                    return;
-                }
-
-                Logger.Info("Configuration loaded successfully.");
-            }
-            catch (Exception ex)
-            {
-                Logger.Error($"Exception occurred while loading configuration: {ex.Message}");
-                _hasConfigurationErrors = true;
-            }
+            if (Config.MessageType == "Broadcast" || Config.MessageType == "Hint" || !Config.MessageContent.IsEmpty()) return;
+            Logger.Error("MessageType is set incorrectly in the config file. Please set it to 'Broadcast' or 'Hint'.");
+            _hasIncorrectSettings = true;
         }
 
-        /// <summary>
-        /// Enable plugin
-        /// Register event handlers and initialize plugin functionality
-        /// </summary>
         public override void Enable()
         {
-            // Check if configuration has errors
-            if (_hasConfigurationErrors)
+            if (_hasIncorrectSettings)
             {
-                Logger.Error("Plugin cannot be enabled due to configuration errors!");
+                Logger.Error("Config format error! The plugin will NOT load");
                 return;
             }
 
-            // Check if plugin is enabled in configuration
-            if (!Config.PluginEnable)
+            Instance = this;
+
+            if (Config.PluginEnable)
             {
-                Logger.Info("Plugin is disabled in configuration.");
-                return;
-            }
-
-            try
-            {
-                // Set plugin instance
-                Instance = this;
-
-                // Register core event handlers (SCP-207 damage exemption)
-                RegisterEventHandlers();
-
-                // Display enable success message
-                DisplayWelcomeMessage();
+                // Register events
+                CustomHandlersManager.RegisterEventsHandler(ExemptDamageEvent);
+                if (Config.MessageEnable) CustomHandlersManager.RegisterEventsHandler(SendMessageEvent);
                 
-                Logger.Info("HarmlessCola plugin has been enabled successfully!");
-            }
-            catch (Exception ex)
-            {
-                Logger.Error($"Failed to enable plugin: {ex.Message}");
-                throw;
+                // Just to show off :)
+                // You can use this website to create your own: https://patorjk.com/software/taag/
+                Logger.Raw("""
+                            _    _                      _                _____      _       
+                           | |  | |                    | |              / ____|    | |      
+                           | |__| | __ _ _ __ _ __ ___ | | ___  ___ ___| |     ___ | | __ _ 
+                           |  __  |/ _` | '__| '_ ` _ \| |/ _ \/ __/ __| |    / _ \| |/ _` |
+                           | |  | | (_| | |  | | | | | | |  __/\__ \__ \ |___| (_) | | (_| |
+                           |_|  |_|\__,_|_|  |_| |_| |_|_|\___||___/___/\_____\___/|_|\__,_|
+                                                                                            
+                           HarmlessCola plugin has been loaded!                                                         
+                           """, ConsoleColor.Cyan);
             }
         }
         
-        /// <summary>
-        /// Disable plugin
-        /// Clean up resources and unregister event handlers
-        /// </summary>
         public override void Disable()
         {
-            try
-            {
-                // Unregister event handlers
-                UnregisterEventHandlers();
-                
-                // Clear instance reference
-                Instance = null;
-                
-                Logger.Info("HarmlessCola plugin has been disabled successfully!");
-            }
-            catch (Exception ex)
-            {
-                Logger.Error($"Error occurred while disabling plugin: {ex.Message}");
-            }
+            // Is this thing really work?
+            Logger.Info("HarmlessCola plugin has been disabled!");
         }
-
-        #endregion
-
-        #region Private Helper Methods
-
-        /// <summary>
-        /// Register event handlers
-        /// Selectively register different event handlers based on configuration
-        /// </summary>
-        private void RegisterEventHandlers()
-        {
-            // Always register damage exemption handler (this is the core functionality of the plugin)
-            CustomHandlersManager.RegisterEventsHandler(ExemptDamageEventHandler);
-            Logger.Debug("Damage exemption event handler registered.", Config.DebugLogEnable);
-
-            // Register message sending handler based on configuration
-            if (Config.MessageEnable)
-            {
-                CustomHandlersManager.RegisterEventsHandler(SendMessageEventHandler);
-                Logger.Debug("Message sending event handler registered.", Config.DebugLogEnable);
-            }
-        }
-
-        /// <summary>
-        /// Unregister event handlers
-        /// Clean up registered event handlers when plugin is disabled
-        /// </summary>
-        private void UnregisterEventHandlers()
-        {
-            try
-            {
-                // Unregister damage exemption handler
-                CustomHandlersManager.UnregisterEventsHandler(ExemptDamageEventHandler);
-                
-                // Unregister message sending handler
-                CustomHandlersManager.UnregisterEventsHandler(SendMessageEventHandler);
-                
-                Logger.Debug("All event handlers have been unregistered.", Config?.DebugLogEnable ?? false);
-            }
-            catch (Exception ex)
-            {
-                Logger.Error($"Failed to unregister event handlers: {ex.Message}");
-            }
-        }
-
-        /// <summary>
-        /// Display welcome message when plugin is enabled
-        /// Contains ASCII art and version information
-        /// </summary>
-        private void DisplayWelcomeMessage()
-        {
-            // ASCII art generation tool: https://patorjk.com/software/taag/
-            string welcomeMessage = $@"
- _    _                      _                _____      _       
-| |  | |                    | |              / ____|    | |      
-| |__| | __ _ _ __ _ __ ___ | | ___  ___ ___| |     ___ | | __ _ 
-|  __  |/ _` | '__| '_ ` _ \| |/ _ \/ __/ __| |    / _ \| |/ _` |
-| |  | | (_| | |  | | | | | | |  __/\__ \__ \ |___| (_) | | (_| |
-|_|  |_|\__,_|_|  |_| |_| |_|_|\___||___/___/\_____\___/|_|\__,_|
-
-HarmlessCola Plugin v{Version.ToString(3)} - Loaded Successfully!
-Author: {Author}
-
-🥤 SCP-207 damage has been disabled!
-💬 Message notifications: {(Config.MessageEnable ? "ON" : "OFF")}
-📝 Logging enabled: {(Config.LogEnable ? "ON" : "OFF")}
-🔧 Debug logging: {(Config.DebugLogEnable ? "ON" : "OFF")}";
-
-            Logger.Raw(welcomeMessage, ConsoleColor.Cyan);
-        }
-
-        #endregion
     }
 }
